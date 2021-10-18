c566b087acd299d51ba8abad95c50ecd9c5b8a76
noura098 <noura.alnami.jiz.s@tuwaiq.edu.sa>
2021-10-18 18:41:29 +0300

    lab10

---
+++ LabTen/LabTen/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  LabTen
+//
+//  Created by NoON .. on 12/03/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  
+    }
+func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+}
