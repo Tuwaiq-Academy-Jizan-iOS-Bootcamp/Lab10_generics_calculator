80db4e6b4bc270b7b41e7a6c166694f3337069bd
JawaherAbulahsah <jawaher.abulahsah.jiz.s@tuwaiq.edu.sa>
2021-10-20 09:20:48 +0300

    other way

--- CalculatorApp/CalculatorApp/ViewController.swift
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -13,45 +13,53 @@ class ViewController: UIViewController {
     @IBOutlet weak var lablelNumber: UILabel!
     
     //creating a variable
-    var numberOnScreen = 0.0
-    var previousNumber = 0.0
+    var numberOnScreen = 0
+    var previousNumber = 0
     var performingMath = false
     var operation = 0
     
+// var mathOperation = ""
+var resetLabel = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
     //connection all number button
     @IBAction func oneButton(_ sender: UIButton) {
+        if resetLabel  {
+            lablelNumber.text = ""
+           // resetLabel = false
+        }
+        resetLabel = false
        if performingMath == true {
         lablelNumber.text = String(sender.tag)
-
-        numberOnScreen = Double(lablelNumber.text!)!
-
+        numberOnScreen = Int(lablelNumber.text!)!
         performingMath = false
 
         }
 
         else {
-
         lablelNumber.text = lablelNumber.text! + String(sender.tag)
-            numberOnScreen = Double(lablelNumber.text!)!
+            numberOnScreen = Int(lablelNumber.text!)!
+           
 
         }
         
     }
     
     //connection for ","
-    @IBAction func buttonActin(_ sender: UIButton) {
-        lablelNumber.text = lablelNumber.text! + "."
-    }
+   // @IBAction func buttonActin(_ sender: UIButton) {
+     //   lablelNumber.text = lablelNumber.text! + "."
+   // }
+    
+    
     
     //connection all operation button
     @IBAction func operationsAction(_ sender: UIButton) {
        if lablelNumber.text != "" && sender.tag != 11 && sender.tag != 16 {
-       previousNumber = Double(lablelNumber.text!)!
+       previousNumber = Int(lablelNumber.text!)!
 
            
         operation = sender.tag
@@ -67,12 +75,13 @@ class ViewController: UIViewController {
             default:
                 lablelNumber.text = "ERROR"
             }*/
+           
            performingMath = true
        }
         
  else if sender.tag == 16 {
 
-            switch operation {
+           /* switch operation {
             case 12:
                 lablelNumber.text = String(previousNumber / numberOnScreen)
             case 13:
@@ -83,16 +92,45 @@ class ViewController: UIViewController {
                 lablelNumber.text = String(previousNumber + numberOnScreen)
             default:
                 lablelNumber.text = "ERROR"
-            }
+            }*/
+     switch operation {
+      case 12:
+          lablelNumber.text = String(divisionFunc(num1: previousNumber, num2: numberOnScreen))
+      case 13:
+          lablelNumber.text = String(multiplicationFunc(num1: previousNumber, num2: numberOnScreen))
+      case 14:
+          lablelNumber.text = String(subtracationFunc(num1: previousNumber, num2: numberOnScreen))
+      case 15:
+          lablelNumber.text = String(addFunc(num1: previousNumber, num2: numberOnScreen))
+      default:
+          lablelNumber.text = "ERROR"
+     }
+     
         }
         
         else if sender.tag == 11{
-        lablelNumber.text = ""
+        lablelNumber.text = "0"
         previousNumber = 0
         numberOnScreen = 0
         operation = 0
+            resetLabel = true
+            
+            
         }
     }
+    func addFunc(num1 : Int ,num2 : Int) -> Int{
+        return num1 + num2
+    }
+    func divisionFunc(num1 : Int ,num2 : Int) -> Int{
+        return num1 / num2
+    }
+    func multiplicationFunc(num1 : Int ,num2 : Int) -> Int{
+        return num1 * num2
+    }
+    func subtracationFunc(num1 : Int ,num2 : Int) -> Int{
+        return num1 - num2
+    }
+    
 }
 
 
