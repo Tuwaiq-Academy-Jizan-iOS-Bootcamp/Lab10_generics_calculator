ccc53afd3361bb81b72751c453f1eb3c2fe853d2
Hanan Somily <hanan.somily.jiz.s@tuwaiq.edu.sa>
2021-10-26 22:32:59 +0300

    update ..

--- Cal/Cal/ViewController.swift
+++ Cal/Cal/ViewController.swift
@@ -6,19 +6,30 @@
 //
 
 import UIKit
+import Foundation
+
 
 class ViewController: UIViewController {
-var number1 = 0
-var number2 = 0
+    var number1:Double = 0
+    var number2:Double = 0
 var operation = ""
 var reseltValue = true
     
     @IBOutlet weak var screen: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func coma(_ sender: UIButton) {
+       // print(sender.currentTitle!)
+        if reseltValue == true {
+            screen.text = "" }
+        screen.text = screen.text! + String(".")
+        reseltValue = false
+        }
+        
     @IBAction func number(_ sender: UIButton) {
         if reseltValue == true {
             screen.text = "" }
@@ -27,16 +38,16 @@ var reseltValue = true
         }
     @IBAction func mathOperation(_ sender: UIButton) {
         switch sender.tag {
-        case 11 : number1 = Int(screen.text!)!
+        case 11 : number1 = Double(screen.text!)!
             operation = "/"
             reseltValue = true
-        case 12 :number1 = Int(screen.text!)!
+        case 12 :number1 = Double(screen.text!)!
             operation = "*"
             reseltValue = true
-        case 13 :number1 = Int(screen.text!)!
+        case 13 :number1 = Double(screen.text!)!
             operation = "+"
             reseltValue = true
-        case 14 :number1 = Int(screen.text!)!
+        case 14 :number1 = Double(screen.text!)!
             operation = "-"
             reseltValue = true
         case 10 :
@@ -51,25 +62,43 @@ var reseltValue = true
     }
     
     @IBAction func equal(_ sender: UIButton) {
-        number2 = Int(screen.text!)!
+        let fmt = NumberFormatter()
+        // Optional:
+        fmt.minimumIntegerDigits = 1
+        fmt.maximumFractionDigits = 4
+        fmt.minimumFractionDigits = 0
+        number2 = Double(screen.text!)!
         if operation == "/"{
-            screen.text = String(number1/number2)
+            screen.text = (fmt.string(from: divide(number1: number1, number2: number2) as NSNumber)!);
             reseltValue = true
         }
         if operation == "*"{
-            screen.text = String(number1*number2)
+            screen.text = (fmt.string(from: multi(number1: number1, number2: number2) as NSNumber)!);
             reseltValue = true
         }
         if operation == "+"{
-            screen.text = String(number1+number2)
+            screen.text = (fmt.string(from:sum(number1: number1, number2: number2) as NSNumber)!);
             reseltValue = true
         }
         if operation == "-"{
-            screen.text = String(number1-number2)
+            screen.text = (fmt.string(from: munse(number1: number1, number2: number2) as NSNumber)!);
             reseltValue = true
         }
       
+        }
+
+    func divide <P:BinaryFloatingPoint>(number1:P,number2:P)->P{
+        return(number1/number2) }
+    func multi <T:Numeric>(number1:T, number2:T)->T{
+        return number1 * number2
     }
+    func sum <T:Numeric>(number1:T , number2:T) -> T {
+        return number1+number2
+    }
+    func munse <T:Numeric>(number1:T, number2:T)->T{
+        return number1-number2
+    }
+
     
 }
 
